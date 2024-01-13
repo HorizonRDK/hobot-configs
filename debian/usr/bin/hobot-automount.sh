602a6557e4340cff368cf0324a854a332099cf96
yaqiang.li <yaqiang.li@horizon.cc>
2024-01-13 16:29:09 +0800

    fix(automount): Fixed the bug that the USB disk may not be automatically mounted when booting
    Signed-off-by: yaqiang.li <yaqiang.li@horizon.cc>

--- debian/usr/bin/hobot-automount.sh
+++ debian/usr/bin/hobot-automount.sh
@@ -4,6 +4,25 @@ pathtoname() {
 	udevadm info -p /sys/"$1" | awk -v FS== '/DEVNAME/ {print $2}'
 }
 
+devices=$(lsblk -l -o NAME -n -p -d)
+
+for device in $devices; do
+	if [[ $device =~ /dev/sd[a-z] ]]; then
+		partitions=$(lsblk -l -o NAME -n -p $device)
+
+		for partition in $partitions; do
+			if [[ $partition =~ /dev/sd[a-z][0-9]+ ]]; then
+				partition_node="/dev/$(basename $partition)"
+				echo "Mounting $partition_node..."
+				for attempt in {1..5}; do
+					udisksctl mount --block-device $partition_node --no-user-interaction && break
+					sleep 1
+				done
+			fi
+		done
+	fi
+done
+
 stdbuf -oL -- udevadm monitor --udev -s block | while read -r -- _ _ event devpath _; do
 	if [ "$event" = add ]; then
 		devname=$(pathtoname "$devpath")
